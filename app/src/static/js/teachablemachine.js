48a1688341abd59f07a748597e5293ff4e3ffe9a
BossHunTerCoding <bhtoobossoo@gmail.com>
2023-03-07 18:47:17 +0700

    edit teachablemachine.js

--- app/src/static/js/teachablemachine.js
+++ app/src/static/js/teachablemachine.js
@@ -2,7 +2,7 @@
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
 // the link to your model provided by Teachable Machine export panel
-const URL = "url_your_model";
+const URL = "https://teachablemachine.withgoogle.com/models/vYeVpTf35/";
 
 let model, webcam, labelContainer, maxPredictions;
 
